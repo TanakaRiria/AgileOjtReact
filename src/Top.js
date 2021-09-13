f76d5f42e6332a6bea6dc33d3bbb37d7dac4abf9
Kazz Nagashima <nagashima_kaz@texnos.co.jp>
2021-09-13 12:58:16 +0900

    Top表示時に/homeへ強制遷移するよう修正

--- src/Top.js
+++ src/Top.js
@@ -1,9 +1,20 @@
-function TopApp() {
+import React, {useState, useEffect} from 'react'
+
+/*
+Topコンポーネント
+*/
+export default function TopApp(props) {
+  // hook
+  useEffect(() => {
+    // homeに遷移
+    props.history.push("/home");
+  },[]); 
+
+  // コンポーネントを描画
   return (
+    // <div onLoad={ toHome }>
     <div>
        <h1>Top</h1>
     </div>
   );
 }
-
-export default TopApp;
